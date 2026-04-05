0d8098787ffa9bc8bfd5f86ecec9ff8815c23e7e
ruasonid9 <122701362+ruasonid9@users.noreply.github.com>
2026-04-05 15:21:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -163,6 +163,7 @@ with st.sidebar:
     )
     max_results = st.slider("Max cases to fetch", min_value=100, max_value=10000, value=1000, step=200)
     st.divider()
+    st.caption("Created by Luc Vulliez.")
     st.caption("Data source: The Free Law Project. CourtListener API. Available from https://www.courtlistener.com/api/")
     st.caption("Fetches matching opinions based on user-inputted parameters.")
 
